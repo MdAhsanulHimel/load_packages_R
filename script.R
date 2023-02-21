d5f2cf2ded4d76c8f9f5ae321343cc91341b2382
Md Ahsanul Islam <67047779+MdAhsanulHimel@users.noreply.github.com>
2023-02-21 15:57:15 +0600

    removed a useless line

--- script.R
+++ script.R
@@ -1,17 +1,11 @@
-
-# defining the function
 load_packages <- function(packages) {
-  
   # check if packages are installed
-  installed_packages <- packages %in% rownames(installed.packages()) 
-  not_installed <- pkges[!installed_packages]
-  
+  not_installed <- packages[!packages %in% rownames(installed.packages())]
   # initiate installation part if any of the packages is not installed
   if(length(not_installed)>0){
     message("Installing packages:", paste(" ", not_installed))
     install.packages(not_installed) |> suppressMessages()
   }
-  
   # load all packages
   message("Loading packages:", paste(" ", packages))
   lapply(X = packages, FUN = library, character.only = TRUE) |> invisible()
