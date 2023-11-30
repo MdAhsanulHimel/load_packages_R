c38ad3de6357701b104e5e81ab1a45eddec27792
Md Ahsanul Islam <67047779+MdAhsanulHimel@users.noreply.github.com>
2023-11-30 19:05:48 +0600

    Do not show loading package(s) message if package is already loaded

--- script.R
+++ script.R
@@ -1,12 +1,23 @@
 load_packages <- function(packages) {
+  
   # check if packages are installed
   not_installed <- packages[!packages %in% rownames(installed.packages())]
-  # initiate installation part if any of the packages is not installed
+  # check packages that are not already loaded but installed
+  not_loaded_packages <- packages[!(packages %in% .packages()) & 
+                                   (packages %in% rownames(installed.packages()))]
+  
   if(length(not_installed)>0){
+    # initiate installation part if any of the packages is not installed
     message("Installing packages:", paste(" ", not_installed))
     install.packages(not_installed) |> suppressMessages()
+    message("Loading installed package(s):", paste(" ", not_installed))
+    lapply(X = not_installed, FUN = library, character.only = TRUE) |> invisible()
   }
-  # load all packages
-  message("Loading packages:", paste(" ", packages))
-  lapply(X = packages, FUN = library, character.only = TRUE) |> invisible()
+  
+  if(length(not_loaded_packages)>0){
+    # initiate loading part if packages are not loaded
+    message("Loading package(s):", paste(" ", not_loaded_packages))
+    lapply(X = packages, FUN = library, character.only = TRUE) |> invisible()
+  }
 }
+
